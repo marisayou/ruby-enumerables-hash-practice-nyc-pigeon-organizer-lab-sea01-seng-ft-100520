459499a9e6a1839b3941a71878220adceac0e83a
Marisa You <marisa.you@gmail.com>
2020-09-01 21:12:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  
+  data.each do |attribute, value|
+    value.each do |attr_value, names|
+      for i in 0...names.length 
+        if !new_data.key?(names[i])
+          new_data[names[i]] = {attribute => [attr_value.to_s]}
+        elsif !new_data[names[i]].key?(attribute)
+          new_data[names[i]][attribute] = [attr_value.to_s]
+        else
+          new_data[names[i]][attribute].push(attr_value.to_s)
+        end
+      end
+    end
+  end
+  
+  return new_data
 end
